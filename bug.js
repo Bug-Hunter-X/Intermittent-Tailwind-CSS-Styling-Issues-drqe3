20e3fb27e8d2ca5d66175fb4b34f5c6c52632d94
BugHunter-X <askzenai@gmail.com>
2024-12-22 23:58:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// ... your Tailwind CSS classes ...
+<div class="bg-gray-100 p-4">
+  <h1 class="text-3xl font-bold text-gray-900">Unexpected Error!</h1>
+  <p>Tailwind CSS classes not working or behaving as expected.</p>
+</div>
+```
